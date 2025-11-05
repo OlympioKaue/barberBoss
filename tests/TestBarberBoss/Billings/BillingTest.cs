89e4e36fb4bc220469b16021d50d1c2c7b47d061
Kaue Olympio da Silva <kaueolympio@outlook.com>
2025-11-05 01:58:22 -0300

    Inserindo testes unitários

---
+++ tests/TestBarberBoss/Billings/BillingTest.cs
@@ -0,0 +1,46 @@
+﻿using barberBoss.Application.UseCases.BillingsUseCase.Validation;
+using barberBoss.Communication.Enum;
+using barberBoss.Communication.Request.Billings;
+using Shouldly;
+using UnitFakerTests.Request;
+
+namespace TestBarberBoss.Billings;
+
+public class BillingTest
+{
+
+    [Fact]
+    public void RequestSucess()
+    {
+        var validator = GetValidator();
+        var builder = BillingFaker.FakerBilling();
+        builder.ServiceName = "Corte de Cabelo";
+        builder.Amount = 35.00m;
+
+        var result = validator.Validate(builder);
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Request_Errors()
+    {
+        var validator = GetValidator();
+        var builder = GetRequestBilling();
+
+        var result = validator.Validate(builder);
+        result.IsValid.ShouldBeFalse();
+    }
+
+      
+
+    private ValidationRegisterBillingsUseCase GetValidator()
+    {
+        return new ValidationRegisterBillingsUseCase();
+    }
+
+    //A requisição tera retorno null/0.
+    private RequestBillings GetRequestBilling()
+    {
+        return new RequestBillings();
+    }
+}
---
+++ tests/UnitTests/Request/BillingFaker.cs
@@ -0,0 +1,18 @@
+﻿using barberBoss.Communication.Enum;
+using barberBoss.Communication.Request.Billings;
+using Bogus;
+
+namespace UnitFakerTests.Request;
+
+public class BillingFaker
+{
+    public static RequestBillings FakerBilling()
+    {
+        var faker = new Faker<RequestBillings>()
+            .RuleFor(src => src.ClientName, faker => faker.Name.FindName())
+            .RuleFor(src => src.BarberName, faker => faker.Name.FindName())
+            .RuleFor(src => src.PaymentMethod, faker => (PaymentMethod)faker.Random.Int(2, 5));
+
+        return faker.Generate();
+    }
+}
